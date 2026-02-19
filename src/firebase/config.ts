9f4f5f5e5ce587c50f5f48d4390916fe44c4f5c5
GRISALIS TECHNOLOGIES <contacto.grisalistech@gmail.com>
2026-02-19 16:20:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8313221829-4f007",
+  "appId": "1:813044109176:web:6e56448fa0037030e5c28d",
+  "apiKey": "AIzaSyCco28silYsrbRP1LGATl9Fs_J54RJPc7k",
+  "authDomain": "studio-8313221829-4f007.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "813044109176"
+};
